05abfe779070b1871f5d64c594d8768084bbf0be
Clint Edwards <cedwards1080@gmail.com>
2021-07-30 16:26:21 -0600

    Adding right answers for the Password checker

--- password.js
+++ password.js
@@ -3,5 +3,23 @@ console.log(`Welcome to the Password validator tool`)
 console.log('Please create a password')
 
 function passwordValidator() {
-    
+
 }
+
+
+///////////////////////////////////ADDED IN REVIEW/////////////////////
+
+// const readline = require("readline");
+
+// const reader = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+// console.log(`Welcome to the Password validator tool`);
+// reader.question(`Please enter your password`, function(answer) {
+//     if(answer.length > 10){
+//         console.log(`Password entered was successfully`)
+//     } else {
+//         console.log(`Password incorrect. Failed.`)
+//     }
+// })
