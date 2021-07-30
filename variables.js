d0f6a81906b2fbf920a05dc19453e94141208f4a
Clint Edwards <cedwards1080@gmail.com>
2021-07-30 14:50:43 -0600

    time saver with one line else statement

--- variables.js
+++ variables.js
@@ -22,9 +22,8 @@ const lovesCode = true
 
 if(faveNum === 13) {
   console.log(`lucky 13!`)
-} else {
-  console.log(`not lucky 13`)
-}
+} else console.log(`not lucky 13`)
+
 
 //////////////////PROBLEM 5////////////////////
 /*
